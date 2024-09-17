af678c5dc155035108a1b19c5fa7fd437b7a5d27
AobaIwaki <aobaiwaki@gmail.com>
2024-09-17 17:14:56 +0900

    Ruffで整形

--- que.py
+++ que.py
@@ -1,28 +1,29 @@
 class Que:
     "要素が整数のキューオブジェクト"
+
     def __init__(self, queue: list[int]) -> None:
+        "コンストラクタ"
         self._queue: list = queue
 
-    def __eq__(self, other)->bool:
+    def __eq__(self, other) -> bool:
         "入力されたキューとの等価性を評価する"
-        if isinstance(other, Que): # otherがQueである
-            return self.get() == other.get() # self, otherのlistが等しい
+        if isinstance(other, Que):  # otherがQueである
+            return self.get() == other.get()  # self, otherのlistが等しい
         return False
 
-
-    def get(self)->list[int]:
+    def get(self) -> list[int]:
         "リストを返す"
         return self._queue
-    
-    def head(self)->int:
+
+    def head(self) -> int:
         "先頭を返す"
         return self._queue[0]
-    
-    def add(self, x:int):
+
+    def add(self, x: int):
         que_list: list[int] = self.get()
         que_list.append(x)
         return Que(que_list)
-    
+
     def pop(self):
         que_list: list[int] = self.get()
         new_que_list = que_list[1:]
